26978187a49005a737b3a389a39ab484a974630e
tamdoer <tamara.doerfer@web.de>
2024-05-08 16:24:14 +0200

    Commiting basic python syntax

--- Basic_python_syntax.py
+++ Basic_python_syntax.py
@@ -1,2 +1,30 @@
-t = 1 + 1
-t
+# Showing that normal math works
+print(1 + 1) # using our first function, print(), the argument is in round brakets
+			 # need to use print() to actually see output on terminal
+
+2/3
+2 * 5
+
+#defining variables
+a = 2
+b = 3
+a*b
+
+c = 2.5 # this is a float
+a * c
+
+"abc2" # this is a string
+d = "abc2"
+e = "4"
+f = 4*e
+
+print(f) #resulting is mathematically wrong, as we are multiplying a string
+
+
+#You can execute a single line or several lines in a script by "activating" them and using:
+#
+#	 option – shift – e on Mac
+#	 alt – shift – e on Windows
+#
+# code will be executed in python console and you can inspect the different variables in the
+# variable explorer then
