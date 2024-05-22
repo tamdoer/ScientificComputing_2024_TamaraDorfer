39f841f319f4bf5ca8fa046abe5c8166d7694fd8
tamdoer <tamara.doerfer@web.de>
2024-05-22 16:56:32 +0200

    Updating (lists, loops)

--- Basic_python_syntax.py
+++ Basic_python_syntax.py
@@ -28,3 +28,35 @@ print(f) #resulting is mathematically wrong, as we are multiplying a string
 #
 # code will be executed in python console and you can inspect the different variables in the
 # variable explorer then
+
+## Introducing lists
+integer_list = [1,2,3,4,5]
+mixed_type_list = ["a",1,1.23]
+print(integer_list)
+
+
+#for loop syntax
+#for x in iterable_item:
+#   your action with x
+#important to include colon
+#indent with 4 empty spaces
+
+for i in integer_list:
+    print(i*2)
+print(i)
+# if we now only use print(i), python will only print the last variable that was i --> 5 in this case
+
+fishies = ["halibut", "cod", "plaice", "shark"]
+for fish in fishies:
+    print(fish)
+
+print(fishies[1])
+#WATCH OUT: python starts counting at 0 so for halibut index 0
+
+print(fishies[-1])
+#when counting from the back, -1 does actually chose the last one (kinda duh, you can't have -0)
+
+print(fishies[2:4])
+#prints number the first number and the one BEFORE the last number
+
+print(integer_list[:3])
