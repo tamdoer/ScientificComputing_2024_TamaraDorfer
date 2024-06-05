f1e641cc2d1335d21aef69fc5f1413a230ef299d
tamdoer <tamara.doerfer@web.de>
2024-06-05 15:28:09 +0200

    Some notes from class

---
+++ Session_290524.py
@@ -0,0 +1,10 @@
+# Recap
+    # lists and for-loop basics
+            list = [1,2,3]
+            for x in list:
+                print(x) # indentation by 4 empty spaces, aka 1 tab
+
+    # took a look at real CTD files
+        # header can be problematic bc different lengths in each file
+        # demonstated a way to deal with this using for-loop and basic python syntax
+
